6aa85ce08cceb4428bbf5321d449dcafe6a879bd
root <root@debian>
2016-05-03 02:23:33 -0700

    Test added in Calculator Test

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -3,9 +3,16 @@ import org.junit.Test;
 
 public class CalculatorTest {
   @Test
-  public void evaluatesExpression() {
+  public void evaluatesExpressionForPlus() {
     Calculator calculator = new Calculator();
     int sum = calculator.evaluate("1+2+3");
     assertEquals(6, sum);
+  }
+
+  @Test
+  public void evaluatesExpressionForMinus() {
+    Calculator calculator = new Calculator();
+    int sum = calculator.evaluate("1+2+13-8");
+    assertEquals(8, sum);
   }
 }
